447c9c8283ae4e408defa4d9d014befc685e1466
Oliver-makes-code <kookooloopdaking@gmail.com>
2021-12-22 13:05:50 -0600

    Add attachment support

--- src/index.ts
+++ src/index.ts
@@ -15,23 +15,33 @@ const channels: Keys = {
     revolt: "01FEXPHJH4WSVXFG12SVJV17G7"
 }
 
+let discordWebhook: discord.Webhook;
+
 const discordClient = discordBot.start(keys.discord);
 const revoltClient = revoltBot.start(keys.revolt);
 
 discordClient.on("messageCreate", msg => {
     if (msg.channelId != channels.discord) return;
-    if (msg.webhookId || msg.author.bot) return;
-    let sent = false
+    if (discordWebhook && msg.webhookId == discordWebhook.id) return;
+    let sent = false;
     revoltClient.channels.fetch(channels.revolt).then(channel => {
         if (sent) return;
-        channel.sendMessage(msg.member?.displayName + ": " + msg.cleanContent);
+        let attach = msg.attachments.map(a => a.url);
+        let dat = msg.member?.displayName + ": " + msg.cleanContent
+        if (dat) channel.sendMessage(dat);
+        for (let i of attach)
+            channel.sendMessage(i);
         sent = true;
     });
 });
 
+function sleep(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 revoltClient.on("message", msg => {
     if (msg.channel_id != channels.revolt) return;
-    if (msg.author?.bot) return;
+    if (msg.author?._id == revoltClient.user?._id) return;
     discordClient.channels.fetch(channels.discord).then(c => {
         if (!c?.isText) return;
         let channel: discord.TextChannel = c as discord.TextChannel;
@@ -40,6 +50,7 @@ revoltClient.on("message", msg => {
             let sent = false;
             for (let hook of hooks) {
                 if (hook.owner?.id != discordClient.user?.id) continue;
+                discordWebhook = hook;
                 let content = msg.content.toString();
                 for (let i of msg.mention_ids? msg.mention_ids: []) {
                     let user = await revoltClient.users.fetch(i);
@@ -52,10 +63,16 @@ revoltClient.on("message", msg => {
                     let channel = await revoltClient.channels.fetch(id);
                     content = content.replace("<#"+id+">", "#"+channel.name);
                 }
+                let attach: string[] = [];
+                for (let i of msg.attachments? msg.attachments: []) {
+                    attach.push("https://autumn.revolt.chat/attachments/"+i._id+"/"+i.filename);
+                }
+                sleep(50);
                 hook.send({
-                    content,
+                    content: content? content: undefined,
                     username: (msg.member?.nickname? msg.member?.nickname: msg.author?.username) + " | Revolt.chat",
-                    avatarURL: msg.member?.generateAvatarURL()? msg.member?.generateAvatarURL(): msg.author?.generateAvatarURL()? msg.author?.generateAvatarURL(): msg.author?.defaultAvatarURL
+                    avatarURL: msg.member?.generateAvatarURL()? msg.member?.generateAvatarURL(): msg.author?.generateAvatarURL()? msg.author?.generateAvatarURL(): msg.author?.defaultAvatarURL,
+                    files: attach
                 });
                 sent = true;
                 break;
